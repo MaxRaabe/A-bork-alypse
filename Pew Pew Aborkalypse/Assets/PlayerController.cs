6cffa4d8f7f028bf41429758de48c7f1dcf89d10
Maximilian Raabe <maximilian_raabe@icloud.com>
2019-08-15 22:27:15 +0200

    [GEN] created Project + imported Incontrol

---
+++ Pew Pew Aborkalypse/Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PPA
+{
+    public class PlayerController : MonoBehaviour
+    {
+        #region Variables
+        [Header("References")]
+        private GameObject r_player;
+        public Rigidbody2D r_rb;
+        #endregion
+
+        // Start is called before the first frame update
+        void Start() {
+            r_player = this.gameObject;
+            r_rb = r_player.GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
---
+++ Pew Pew Aborkalypse/Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+namespace PPA
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        public PlayerController r_controller;
+        private InputDevice r_joystick;
+
+        private void Awake() {
+            r_joystick = InputManager.ActiveDevice;
+        }
+
+        // Update is called once per frame
+        void Update() {
+            
+            
+
+        }
+    }
+}
