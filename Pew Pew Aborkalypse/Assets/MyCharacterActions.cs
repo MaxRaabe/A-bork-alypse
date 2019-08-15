73796db6e325f47a9b9b46ece9d1990adb9b7ce2
Maximilian Raabe <maximilian_raabe@icloud.com>
2019-08-15 23:45:12 +0200

    [PRO] tried to set up InControl actions (W.I.P.)

---
+++ Pew Pew Aborkalypse/Assets/MyCharacterActions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class MyCharacterActions : PlayerActionSet
+{
+    public PlayerAction Left;
+    public PlayerAction Right;
+    public PlayerAction Jump;
+    public PlayerAction Shoot;
+    public PlayerAction Sword;
+    public PlayerAction Dash;
+
+    public  MyCharacterActions() {
+        Left = CreatePlayerAction ("Move Left");
+        Right = CreatePlayerAction ("Move Right");
+        Jump = CreatePlayerAction ("Jump");
+        Shoot = CreatePlayerAction ("Shoot");
+        Sword = CreatePlayerAction ("Sword Swing");
+        Dash = CreatePlayerAction ("Dash");
+    }
+    
+}
--- Pew Pew Aborkalypse/Assets/PlayerMovement.cs
+++ Pew Pew Aborkalypse/Assets/PlayerMovement.cs
@@ -9,16 +9,36 @@ namespace PPA
     {
         public PlayerController r_controller;
         private InputDevice r_joystick;
+        MyCharacterActions r_characterActions;
 
-        private void Awake() {
+        private void Start() {
             r_joystick = InputManager.ActiveDevice;
+            r_characterActions = new MyCharacterActions();
+
+            r_characterActions.Left.AddDefaultBinding(InputControlType.LeftStickLeft);
+
+            r_characterActions.Right.AddDefaultBinding(InputControlType.LeftStickRight);
+
+            r_characterActions.Jump.AddDefaultBinding(InputControlType.Action1);
+
+            r_characterActions.Shoot.AddDefaultBinding(InputControlType.RightTrigger);
+            r_characterActions.Shoot.AddDefaultBinding(InputControlType.LeftTrigger);
+
+            r_characterActions.Sword.AddDefaultBinding(InputControlType.Action3);
+
+            r_characterActions.Dash.AddDefaultBinding(InputControlType.RightBumper);
+            r_characterActions.Dash.AddDefaultBinding(InputControlType.LeftBumper);
         }
 
         // Update is called once per frame
         void Update() {
+            if(r_characterActions.Left.IsPressed)
+                MoveLeft();
             
-            
+        }
 
+        void MoveLeft() {
+            r_controller.r_rb.AddForce(r_joystick.LeftStick,0);
         }
     }
 }
